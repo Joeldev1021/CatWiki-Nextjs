296ca73993af524e7c00fdc2e63e07922c7db1ff
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-04-16 17:29:54 -0500

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:jsx-a11y/recommended',
+    'next',
+    'next/core-web-vitals',
+  ],
+  rules: {
+    'semi': ['error', 'always'],
+  }
+}; 
