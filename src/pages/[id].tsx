e4b3d62e101de66a90cd6f4e6372c15a0df826f6
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-04-28 08:18:58 -0500

    feat: page dynamic for id

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,45 +1,63 @@
-/* eslint-disable no-unused-vars */
-import { GetStaticPaths, GetStaticProps } from 'next';
-import { ParsedUrlQuery } from 'querystring';
-import React from 'react';
+import About from "@components/About/About";
+import Layout from "@containers/Layout";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { ParsedUrlQuery } from "querystring";
+import React from "react";
 
 interface Params extends ParsedUrlQuery {
-    id: string;
+  id: string;
 }
-interface Props {
-    id: string;
+
+interface CatProps {
+  data: any;
 }
+
 // This function gets called at build time on server-side.
-export const getStaticProps: GetStaticProps<Params> = async ({params}) => {
-    const {id} = params as Params;
-    console.log("id", id);
+export const getStaticProps: GetStaticProps<CatProps, Params> = async ({
+  params,
+}) => {
+  const { id } = params as Params;
+  const res = await fetch(`${process.env.API_URL}/breeds/${id}`, {
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": `${process.env.API_KEY}`,
+    },
+  });
+  const data = await res.json();
+  if (!data) {
     return {
-        props: {
-            id
-        }
+      notFound: true,
     };
-}; 
+  }
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
 // This gets called on every request
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
- const result = await fetch(`https://api.thecatapi.com/v1/breeds`, {
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": `${process.env.API_KEY}`,
-        },
-   }); 
-    const data = await result.json();
-    return {
-        paths: data.map((item: any) => ({params: {id: item.id}})),
-        fallback: "blocking"
-    };
+  const result = await fetch(`https://api.thecatapi.com/v1/breeds`, {
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": `${process.env.API_KEY}`,
+    },
+  });
+
+  const data = await result.json();
+  return {
+    paths: data.map((item: any) => ({ params: { id: item.id } })),
+    fallback: "blocking",
+  };
 };
 
-const  CatComponent = ({ id }:Props) => {
+const CatComponent = ({ data }: CatProps) => {
   return (
-      <div>
-          <h1>hola-{id}</h1>
-      </div>
+    <Layout>
+      <About data={data}/>
+    </Layout>
   );
 };
 
-export default CatComponent; 
+export default CatComponent;
