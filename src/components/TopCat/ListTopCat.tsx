b55173f517b28e519955c41d7e0f88609ac50408
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-04-28 08:20:09 -0500

    feat: component ListTopCat

---
+++ src/components/TopCat/ListTopCat.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from '@styles/listTopCat.module.scss';
+
+interface Props {
+    img : string;
+    name : string;
+    description : string;
+}
+
+const ListTopCat = ({img, name, description}:Props) => {
+  return (
+      <div className={styles['card-top']}>
+          <div className={styles['card-img']}>
+             <Image src={`https://cdn2.thecatapi.com/images/${img}.jpg`} alt={name} width={170} height={170} layout="responsive" /> 
+          </div>
+          <div>
+              <p className={styles['card-name']}>{name}</p>
+              <p className={styles['card-description']}>{description}</p>
+          </div>
+      </div>
+  );
+};
+
+export default ListTopCat;
