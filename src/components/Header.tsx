24c8797ca81dc4d27630e7cd7817adfd45266983
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-07 08:12:16 -0500

    feat: header Link to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <header>
-      <Image
-        src={"/logo.svg"}
-        alt="Picture of the author"
-        width={100}
-        height={50}
-        priority
-      />
+      <Link href="/">
+        <a>
+          <Image
+            src={"/logo.svg"}
+            alt="Picture of the author"
+            width={100}
+            height={50}
+            priority
+          />
+        </a>
+      </Link>
     </header>
   );
 };
