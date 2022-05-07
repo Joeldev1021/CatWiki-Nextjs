c461721ff97c25a90ecb921426565822b2d6b3a6
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-07 08:08:16 -0500

    feat: hook useFech

--- src/pages/more/index.tsx
+++ src/pages/more/index.tsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from "react";
+import Loading from "@components/Loading";
+import TopCat from "@components/TopCat/TopCat";
+import Layout from "@containers/Layout";
+import useFetch from "hooks/useFetch";
+import { NextPage } from "next";
 
-const More = () => {
-
-  const [topResult, setTopResult] = useState<any[]>([]);
-   
-  useEffect(() => {
-    fetch('https://api.thecatapi.com/v1/breeds?limit=10&page=0', {
-        headers: {
-            "Content-Type": "application/json",
-            "x-api-key": `${process.env.API_KEY}`,
-        },
-    })
-    .then(res => res.json())
-    .then(res => setTopResult(res));
-  }, []);
+const More:NextPage = () => {
+ 
+  const {data} = useFetch({url: 'https://api.thecatapi.com/v1/breeds?limit=10&page=0'});
 
   return ( 
-    <div>
-      {
-        topResult.map((item: any, index:number) => <h1 key={item.id}>{index+1}-{item.name}</h1>) 
-      }
-    </div>
+    <Layout>
+       {data.length > 0 ? <TopCat topCat={data} /> : <Loading />}
+    </Layout>
   );
 };
 
