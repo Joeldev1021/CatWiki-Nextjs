d0c03bc4e751e08c861bfb69a25c121e2e7aa770
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-07 08:11:27 -0500

    feat: suggestion search

--- src/components/Suggesions.tsx
+++ src/components/Suggesions.tsx
@@ -1,20 +1,30 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import styles from "@styles/Suggesions.module.scss";
+import Link from "next/link";
 
 interface Props {
   inputData: string;
   listCat: any[];
 }
 const Suggesions = ({ inputData, listCat }: Props) => {
+  
   return (
-    <div className={styles["suggestions"]}>
-      {inputData.length > 0 &&
-        listCat.map((item: any) => {
-          if (item.name.toLowerCase().includes(inputData.toLowerCase())) {
-            return <li key={item.id}>{item.name}</li>;
-          }
-        })}
-    </div>
+    <>
+      {inputData.length > 0 && (
+        <div className={styles["suggestions"]}>
+          {listCat.map((item: any) => {
+            if (item.name.toLowerCase().includes(inputData.toLowerCase())) {
+              return (
+                <Link href={`/${item.id}`} key={item.id}>
+                  <a>{item.name}</a>
+                </Link>
+              );
+            }
+          })}
+        </div>
+      )}
+    </>
   );
 };
 
