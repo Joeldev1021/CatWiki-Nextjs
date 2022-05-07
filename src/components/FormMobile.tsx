21b812272541cb46f4d0f0d335c5690bfe3b82f3
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-07 08:18:07 -0500

    feat: formMobile function

--- src/components/FormMobile.tsx
+++ src/components/FormMobile.tsx
@@ -1,14 +1,37 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useRef } from "react";
 import styles from "@styles/FormMobile.module.scss";
+import Suggesions from "./Suggesions";
 
-const FormMobile:React.FC = () => {
+interface Props {
+  handleSearch: (n:string) => void;
+  listCat: any[];
+  setIsShowFormMobile: (n:boolean) => void;
+  setInputData: (n:string) => void;
+  inputData : string;
+}
+
+const FormMobile:React.FC<Props> = ({inputData,setInputData, listCat, setIsShowFormMobile}) => {
+  const inputRef =  useRef<HTMLInputElement>(null);
+  
+ /* A hook that is called when the component is mounted. It is used to focus the input element. */
+  useEffect(() => {
+    inputRef.current?.focus(); 
+  }, []);
+
+  const handleClose = () => {
+    setIsShowFormMobile(false);
+    setInputData("");
+  };
+  
   return (
     <div className={styles['form-modal']}>
-        <button className={styles['btn-close']}>x</button>
+        <button onClick={()=> handleClose()} className={styles['btn-close']}>x</button>
       <form>
-        <input placeholder="Search" type="text" />
+        <input ref={inputRef} defaultValue={inputData} onChange={(e)=> setInputData(e.target.value)}  placeholder="Search" type="text" />
         <button><i className="fa-solid fa-magnifying-glass"></i></button>
       </form>
+      <Suggesions listCat={listCat} inputData={inputData} />
     </div>
   );
 };
