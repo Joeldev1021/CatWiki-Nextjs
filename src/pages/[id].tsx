e0d80da5c9a595930eaa5bb10ec219ee73293f5a
Joel-Hendrix1021 <joelpimentel1995@gmail.com>
2022-05-07 08:09:42 -0500

    feat:add loading

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,8 +1,11 @@
 import About from "@components/About/About";
 import Layout from "@containers/Layout";
-import { GetStaticPaths, GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps} from "next";
+import type { NextPage } from "next";
 import { ParsedUrlQuery } from "querystring";
 import React from "react";
+import { config } from "utils";
+import Loading from "@components/Loading";
 
 interface Params extends ParsedUrlQuery {
   id: string;
@@ -17,12 +20,10 @@ export const getStaticProps: GetStaticProps<CatProps, Params> = async ({
   params,
 }) => {
   const { id } = params as Params;
-  const res = await fetch(`${process.env.API_URL}/breeds/${id}`, {
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": `${process.env.API_KEY}`,
-    },
+  const res = await fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${id}`, {
+    headers:config.header 
   });
+
   const data = await res.json();
   if (!data) {
     return {
@@ -36,15 +37,13 @@ export const getStaticProps: GetStaticProps<CatProps, Params> = async ({
     },
   };
 };
+
 // This gets called on every request
 export const getStaticPaths: GetStaticPaths<Params> = async () => {
   const result = await fetch(`https://api.thecatapi.com/v1/breeds`, {
-    headers: {
-      "Content-Type": "application/json",
-      "x-api-key": `${process.env.API_KEY}`,
-    },
+    headers: config.header,
   });
-
+  
   const data = await result.json();
   return {
     paths: data.map((item: any) => ({ params: { id: item.id } })),
@@ -52,10 +51,14 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
   };
 };
 
-const CatComponent = ({ data }: CatProps) => {
+const CatComponent:NextPage<CatProps> = ({ data }) => {
   return (
     <Layout>
-      <About data={data}/>
+      {
+        data.length > 0
+        ? <About image={data[0].url} data={data[0].breeds[0]}/>
+        :<Loading/>
+      }
     </Layout>
   );
 };
